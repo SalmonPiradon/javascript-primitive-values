67c327457788a3cafac5d5a4f4101ceb937d791e
Bell_Teerapat <wolfman13bell@gmail.com>
2026-06-02 16:12:12 +0700

    feat: upload ex-javascript-primitive-values

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalAllPrice = 27000;
+let discount = totalAllPrice * 0.25;
+let totalPrice = totalAllPrice - discount;
+console.log(totalPrice);
