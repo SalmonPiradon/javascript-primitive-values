5a2d5217d9f488abd3388b642be114ec4732b454
Piradon <piradonleungamornnara@gmail.com>
2026-06-02 14:43:28 +0700

    finish exercise1-7

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discount = 0.25;
+let finalPrice = totalPrice - (totalPrice * discount);
+console.log(finalPrice);
